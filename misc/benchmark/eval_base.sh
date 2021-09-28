051056c8068a3de6f99e2b7f12b957a44ff621ae
guanyi.gl <guanyi.gl@alibaba-inc.com>
2021-09-28 16:58:46 +0800

    upload benchmark scripts

---
+++ misc/benchmark/eval_base.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+dataset_prefix=/root/data/liang/new_dataset
+gunrock_prefix=/root/data/liang/gunrock/build/bin
+groute_prefix=/root/data/liang/groute/build
+grape_prefix=/root/data/liang/libgrape-lite_pwrliang/build
+grape_ser_prefix=/root/data/liang/ser
+dataset_source_node=('soc-LiveJournal1' '1'
+  'soc-twitter-2010' '1'
+  'uk-2002' '1'
+  'uk-2005' '1'
+  'webbase-2001' '1'
+  'hollywood-2009' '1'
+  'soc-sinaweibo' '1'
+  'soc-orkut' '1'
+  'indochina-2004' '1'
+  'arabic-2005' '1'
+  'kron_g500-logn21' '1'
+  'delaunay_n24' '1'
+  'cit-Patents' '1'
+  'road_usa' '1'
+  'europe_osm' '1')
